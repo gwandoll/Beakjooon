5229b7e0e0d17d73d4bd915a514246d56d9e63c6
gwandoll <129955868+gwandoll@users.noreply.github.com>
2023-04-07 19:30:23 +0900

    [Silver I] Title: 경로 찾기, Time: 180 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,15 @@
+n = int(input())
+graph = [[0] * (n) for _ in range(n)]
+
+for i in range(n):
+    graph[i] = list(map(int,input().split()))
+               
+for k in range(n):
+    for a in range(n):
+        for b in range(n):
+            if graph[a][b] == 1 or (graph[a][k] == 1 and graph[k][b]== 1):
+                graph[a][b] =1
+for i in range(n):
+    for j in range(n):
+        print(graph[i][j], end = ' ')
+    print('')
